9716f91dacb1f4692ebc3b8cc62f41285b6c724f
Mike Lucek <michael.s.lucek@gmail.com>
2015-11-22 18:10:55 -0500

    submit instead of click for form submit. fixed render issue on chat when no buddy is present

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -4,15 +4,20 @@ $(document).ready(function(){
 		$('#chat').scrollTop($('#chat')[0].scrollHeight);
 	}, 2000)
 
-$(".chat-submit").click(function(){
-	//let's give the data a moment to submit before clearing the form.
-	//probably a race condition
-	setTimeout(function(){
-		$("#message").val("");
-	}, 1000)
+// $(".chat-submit").click(function(){
+// 	//let's give the data a moment to submit before clearing the form.
+// 	//probably a race condition
+// 	setTimeout(function(){
+// 		$("#message").val("");
+// 	}, 1000)
 	
+	$(".chat-form").submit(function(){
+		setTimeout(function(){
+			$("#message").val("");
+		}, 1000)
+	})
 
 });
 
-});
+
 
