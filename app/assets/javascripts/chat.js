5e745bc8a677256307992b006e5c5d0b862589b3
Mike Lucek <michael.s.lucek@gmail.com>
2015-11-30 15:15:48 -0500

    small layout changes

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
-	setInterval(function(){
-		$("#chat").load("/show-message");
-		$('#chat').scrollTop($('#chat')[0].scrollHeight);
-	}, 2000)
+	startSetInterval();	
 
 // $(".chat-submit").click(function(){
 // 	//let's give the data a moment to submit before clearing the form.
@@ -10,7 +7,10 @@ $(document).ready(function(){
 // 	setTimeout(function(){
 // 		$("#message").val("");
 // 	}, 1000)
-	
+	$("#chat").hover(function(){
+		clearInterval(timer);
+	}, function(){startSetInterval();})
+
 	$(".chat-form").submit(function(){
 		setTimeout(function(){
 			$("#message").val("");
@@ -19,5 +19,11 @@ $(document).ready(function(){
 
 });
 
+function startSetInterval(){
+	timer = setInterval(function(){
+		$("#chat").load("/show-message");
+		$('#chat').scrollTop($('#chat')[0].scrollHeight);
+	}, 2000)
 
+}
 
